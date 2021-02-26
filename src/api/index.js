d9df19979db892cdda9ea304c30ef2d2d90f30d2
Glenn Ludszuweit <glenn.ludszuweit@gmail.com>
2021-02-26 15:29:10 +0100

    api to https instead

--- src/api/index.js
+++ src/api/index.js
@@ -5,11 +5,10 @@ const airPollutionApiKey = '40378e26c064ea4354dd1e2f0a5f91be';
 
 export const getMapGeocode = (city) =>
   axios.get(
-    `http://open.mapquestapi.com/geocoding/v1/address?key=${mapApiKey}&inFormat=kvp&outFormat=json&&thumbMaps=false&maxResult=1&location=${city}`
+    `https://open.mapquestapi.com/geocoding/v1/address?key=${mapApiKey}&inFormat=kvp&outFormat=json&&thumbMaps=false&maxResult=1&location=${city}`
   );
 
 export const getAirPollutionData = (lat, lng) =>
   axios.get(
-    `http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lng}&appid=${airPollutionApiKey}`
-    // `http://api.airvisual.com/v2/city?city=${city}&state=California&country=USA&key=82f594ef-fc70-4e3f-963f-4d55e2c6f919`
+    `https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lng}&appid=${airPollutionApiKey}`
   );
